67baed9ebe59a83481264a079a64cd95fa09b77c
AlexanderTorrico <torrico.torrico.alexander@gmail.com>
2025-09-14 15:53:16 -0400

    rm azTable

--- src/components/AzTable/index.js
+++
@@ -1,4 +0,0 @@
-export { default as AzTable } from './AzTable';
-export { default as AzTableColumn } from './AzTableColumn';
-export { default as AzTableHeader } from './AzTableHeader';
-export * as ColumnTypes from './types';

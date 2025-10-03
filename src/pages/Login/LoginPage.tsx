a1fb647729d2b848b560e4c9bf7024b7dc6b951c
AlexanderTorrico <torrico.torrico.alexander@gmail.com>
2025-10-03 10:29:33 -0400

    clear login jsx

--- src/pages/Login/LoginPage.tsx
+++ src/pages/Login/LoginPage.tsx
@@ -1,19 +1,7 @@
-import React from 'react';
 
-import {
-  Row,
-  Col,
-  CardBody,
-  Card,
-  Container,
-} from 'reactstrap';
 import { LoginForm } from './components/LoginForm';
 import { ContainerWrapper } from './components/ContainerWrapper';
 
-// Import images - same as original
-import profile from '../../assets/images/profile-img.png';
-import logo from '../../assets/images/logo.svg';
-import lightlogo from '../../assets/images/logo-light.svg';
 
 const LoginPage = () => {
   // Meta title - same as original
